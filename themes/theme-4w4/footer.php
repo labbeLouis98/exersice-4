89a70f62b788886c1ef4489563f8fd9d64efa86e
Louis Nicolas <shinoclabl@gmail.com>
2021-02-19 17:05:10 -0500

    add lab3 vf

--- themes/theme-4w4/footer.php
+++ themes/theme-4w4/footer.php
@@ -16,7 +16,7 @@
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'theme-4w4' ) ); ?>">
 				<?php
 				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'theme-4w4' ), 'WordPress' );
+				// printf( esc_html__( 'Proudly powered by %s', 'theme-4w4' ), 'WordPress' );
 				?>
 			</a>
 			<span class="sep"> | </span>
