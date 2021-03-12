6ba0de0698fc0a50da78b280c95dffa564ff96a5
Louis Nicolas <shinoclabl@gmail.com>
2021-03-12 14:45:21 -0500

    remplacement de la function php pour les titres les titres sont maintenent classes par num de session

--- themes/theme-4w4/category-cours.php
+++ themes/theme-4w4/category-cours.php
@@ -10,41 +10,45 @@
 get_header();
 ?>
 ///////////////////////////////////////////////////////// CATEGORY-COURS.PHP
-	<main id="primary" class="site-main">
+<main id="primary" class="site-main">
 
-		<?php if ( have_posts() ) : ?>
+    <?php if ( have_posts() ) : ?>
 
-			<header class="page-header">
-				<?php
+    <header class="page-header">
+        <?php
 				the_archive_title( '<h1 class="page-title">', '</h1>' );
 				the_archive_description( '<div class="archive-description">', '</div>' );
 				?>
-			</header><!-- .page-header -->
+    </header><!-- .page-header -->
 
-			<?php
+    <?php
 			/* Start the Loop */
+            $precedent = 0;
 			while ( have_posts() ) :
 				the_post();
+                $titre = get_the_title();
+                $session = substr($titre, 4,1);
+                
 
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
+                ?>
+    <?php
+                if ($session != $precedent){
+                    echo "<p>Session : " . $session . "</p>"; 
+                }
+                $precedent = $session;
+                ?>
+                
+
+    <p> <?php echo $session . " - " . $titre; ?> </p>
+
+    <?php
 
 			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
 
 		endif;
 		?>
 
-	</main><!-- #main -->
+</main><!-- #main -->
 
 <?php
 get_sidebar();
