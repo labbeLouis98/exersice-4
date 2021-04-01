7a2b525e413ba229b22e9c8aaf84e170b480b5ca
Louis Nicolas <shinoclabl@gmail.com>
2021-04-01 16:07:19 -0400

    - index html - svg background - article width

--- themes/theme-4w4/template-parts/content-carrousel.php
+++ themes/theme-4w4/template-parts/content-carrousel.php
@@ -12,14 +12,14 @@
 
 <article class="slide__conteneur">
 	<div class="slide">
-	<img id="svg1" src="https://s2.svgbox.net/illlustrations.svg?ic=programing&color=000000">
+	
 		<div class="slide__info">
 			<p> <?php echo $tPropriete ['sigle'] . " - " . $tPropriete ['nbHeure'] . " - " . $tPropriete ['typeCours']; ?>
 			</p>
 			<a href="<?php echo get_permalink(); ?>"> <?php echo $tPropriete ['titre']; ?> </a>
 			<p> Session <?php echo $tPropriete ['session']; ?> </p>
 		</div>
-		<img id="svg3" src="https://s2.svgbox.net/illlustrations.svg?ic=app-development&color=000000">
+		
 	</div>
 </article>
 
